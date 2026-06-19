3121f4fd32377dcbca66dfd30a4865d797e78134
aruna-bharathi1911 <arunabharathis2005@gmail.com>
2026-06-20 01:07:02 +0530

    updating

---
+++ Day 1/add_two_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    int n1,n2;
+    num1=84;
+    num2=16;
+    printf("Addition: %d \n",num1+num2);
+
+    //user input
+    printf("enter number 1:");
+    scanf("%d",&n1);
+    printf("enter number 2:");
+    scanf("%d",&n2);    
+    printf("Addition of user input :%d\n",n1+n2);
+
+
+    return 0;
+}
