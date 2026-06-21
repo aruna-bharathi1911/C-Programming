f9d39e406669219f6fd8876ac8502c11df763d72
aruna-bharathi1911 <arunabharathis2005@gmail.com>
2026-06-21 13:39:30 +0530

    updated

---
+++ Day 2/typeCasting.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    printf("float %f",(float)a);    // int->float
+
+    char b='A';
+    printf("\nint %d",(int)b);   //char ->int
+
+    int c=3673;
+    printf("\nlong %ld",(long)c);  //int->long
+
+    int d=68;
+    printf("\nchar %c",(char)d);    //int->char 
+
+    return 0;
+
+}
