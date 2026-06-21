0f9050f458a28f591c61ae6ade2ed9c5b94c3662
aruna-bharathi1911 <arunabharathis2005@gmail.com>
2026-06-21 16:41:55 +0530

    arithmetic operations

---
+++ Day 2/arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=17,b=7;
+    printf("ARITHMETIC OPERATIONS");
+
+    printf("\nAddition: %d",a+b);
+    printf("\nSubtraction: %d",a-b);
+    printf("\nMultiplication: %d",a*b);
+    printf("\nDivision: %d",a/b);
+    printf("\nModulus: %d",a%b);
+
+    return 0;
+
+}
